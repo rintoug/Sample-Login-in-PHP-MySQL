8052184391e033c9b8d9463f8b486ff8277392f2
Rinto George <mail.rinto@gmail.com>
2017-04-04 20:31:15 +0400

    Update insert_user.php

--- insert_user.php
+++ insert_user.php
@@ -6,7 +6,11 @@ $password  = password_hash("samplepass123", PASSWORD_BCRYPT);
 $created_at  =date("Y-m-d H:i");
 
 
-$sql = "INSERT INTO `users` (`name`, `email`, `password`, `created_at`) VALUES ('$name', '$email', '$password', '$created_at')";
+$sql = "INSERT INTO `users` (`name`, `email`, `password`, `created_at`) VALUES (:name, :email, :password, :created_at)";
 $stmt = $conn->prepare($sql);		
+$stmt->bindParam(':name', $name);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':password', $password);
+$stmt->bindParam(':created_at', $created_at;
 $stmt->execute(); //This will insert a row
 print "User $name";
